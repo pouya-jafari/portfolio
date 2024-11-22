1e70c793402a9f9c3b8ce9ed6cda1e5682b5ce12
Pouya Jafari <pouya-jafari@outlook.com>
2024-11-22 15:51:03 -0800

    created hero section
    Signed-off-by: Pouya Jafari <pouya-jafari@outlook.com>

---
+++ src/components/Hero.tsx
@@ -0,0 +1,51 @@
+import { FaLocationArrow } from "react-icons/fa";
+import MagicButton from "./ui/MagicButton";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen "
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.01 ] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+      <div className="flex justify-center relative my-20  z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Pouya Jafari
+          </h2>
+
+          <TextGenerateEffect
+            words="Let's get connected and work together!"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Full-Stack Web Developer
+          </p>
+
+          <button className="inline-flex  h-12 animate-shimmer items-center justify-center rounded-3xl border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 hover:scale-105">
+            Resume
+          </button>
+          <MagicButton
+            title="See my Resume"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/ui/MagicButton.tsx
@@ -0,0 +1,30 @@
+type MagicButton = {
+  title: string;
+  icon: React.ReactNode;
+  position: string;
+  handleClick?: () => void;
+  otherClasses?: string;
+};
+
+const MagicButton = ({
+  title,
+  icon,
+  position,
+  handleClick,
+  otherClasses,
+}: MagicButton) => {
+  return (
+    <button className="s relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 md:w-60 md:mt-10 transition transform duration-300 hover:scale-105">
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+      <span
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
+      >
+        {position === "left" && icon}
+        {title}
+        {position === "right" && icon}
+      </span>
+    </button>
+  );
+};
+
+export default MagicButton;
