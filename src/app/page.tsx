4d143c4b8b0fcd14bd6133651dc01369dab7b3eb
pouya-jafari <pouyajefri1080@outlook.com>
2024-11-25 12:57:45 -0800

    Created About me component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import AboutMe from "@/components/Aboutme";
 import Hero from "@/components/Hero";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
       <main className="relative dark:bg-black-100 flex flex-col items-center justify-center mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full md:mb-16 max-sm:mb-2">
           <Hero />
+          <AboutMe />
         </div>
       </main>
     </div>
